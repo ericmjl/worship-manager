eb8eb21dc492e61b9b3b51d687fda89334aef8eb
Eric Ma <ericmajinglong@gmail.com>
2017-07-25 10:50:08 -0400

    Finished songs refactor!

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template, request, redirect
 from tinydb import TinyDB
+from tinydb.operations import delete
 from hanziconv import HanziConv
-from datamodels import Lyrics
-from validation import validate_song
+from datamodels import Song, Lyrics
 
 app = Flask(__name__)
 song_db = TinyDB('song.db')
@@ -11,25 +11,58 @@ calendar_db = TinyDB('calendar.db')
 
 hzc = HanziConv()
 
+# Keep a list of song keys
+song_datamodel = list(Song().to_dict().keys())
+
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
 
-@app.route('/coworkers', methods=['POST'])
-def view_coworkers():
-    return render_template('coworkers.html')
+def get_lyrics(data, exclude_id=None):
+    """
+    Utility function that returns a Lyrics object containing the song lyrics.
+
+    Parameters:
+    ===========
+    - data: (dict-like) pass in `request.form` from Flask app.
+    - exclude_id: (int) used in excluding a particular lyrics section.
+    """
+    # Defensive programming checks
+    if exclude_id:
+        assert isinstance(exclude_id, int)
+
+    # Get lyrics
+    l = Lyrics()
+    for k, v in data.items():
+        if 'section' in k:
+            idx = int(k.split('-')[-1])
+            if idx is not exclude_id:
+                lyrics = hzc.toTraditional(request.form[f'lyrics-{idx}'])
+                section = request.form[k]
+                l.add_section(section=section,
+                              lyrics=lyrics)
+    return l
 
 
-@app.route('/coworkers/<int:id>/view/', methods=['POST'])
-def view_coworker(id):
-    pass
+def update_song_info(request, eid, exclude_id=None):
+    """
+    Updates song information in database.
 
+    Parameters:
+    ===========
+    - request: (dict-like) the `request` object from the Flask app.
+    - eid: (int) the eid of the song to be updated in the database.
+    """
+    data = {k: hzc.toTraditional(v)
+            for k, v in request.form.items()
+            if k in song_datamodel}
+    print(data)
 
-@app.route('/coworkers/<int:id>/edit/', methods=['POST'])
-def edit_coworker(id):
-    pass
+    lyrics = get_lyrics(data=request.form, exclude_id=exclude_id)
+    data['lyrics'] = lyrics.to_dict()
+    song_db.update(data, eids=[eid])
 
 
 @app.route('/songs', methods=['POST'])
@@ -39,54 +72,48 @@ def view_songs():
     return render_template('songs.html', all_songs=all_songs)
 
 
-@app.route('/songs/add', methods=['POST'])
-def new_song():
-    data = dict()
-    eid = song_db.insert(data)
+@app.route('/songs/<int:eid>/view')
+@app.route('/songs/<int:eid>')
+def view_song(eid):
     song = song_db.get(eid=eid)
     return render_template('song.html', song=song, add_section=False)
 
 
-@app.route('/songs/save', methods=['POST'])
-def save_song():
-    data = {k: hzc.toTraditional(v) for k, v in request.form.items()}
-    try:
-        assert data['name'] is not '' and data['source'] is not ''
-        song_db.insert(data)
-    except AssertionError:
-        # We silently pass if 'name' and 'source' are not filled in.
-        pass
+@app.route('/songs/add', methods=['POST'])
+@app.route('/songs/add')
+def new_song():
+    data = Song().to_dict()
+    eid = song_db.insert(data)
+    print(eid)
+    song = song_db.get(eid=eid)
+    return render_template('song.html', song=song)
+
+
+@app.route('/songs/<int:eid>/save', methods=['POST'])
+@app.route('/songs/<int:eid>/save')
+def save_song(eid):
+    update_song_info(request=request, eid=eid)
     return redirect('/songs')
 
 
-
-def update_song_info(eid, exclude_id=None):
-    # Validation
-    data = {k: v for k, v in request.form.items()}
-
-    l = Lyrics()
-    for k, v in data.items():
-        if 'section' not in k or 'lyrics' not in k:
-            song_db.update({k: hzc.toTraditional(v)}, eids=[eid])
-        # This conditional checks that we have sections that we want to keep.
-        if 'section' in k and int(k.split('-')[-1]) is not exclude_id:
-            l.add_section(request.form[k],
-                          request.form[f'lyrics-{k.split("-")[-1]}'])
-    song_db.update({'lyrics': l.sections}, eids=[eid])
-
-
-@app.route('/songs/<int:eid>/update/', methods=['POST'])
-def update_song(eid):
-    update_song_info(eid)
+@app.route('/songs/<int:eid>/remove', methods=['POST'])
+def delete_song(eid):
+    song_db.remove(eids=[eid])
     return redirect('/songs')
 
 
-@app.route('/songs/<int:eid>/add_lyrics_section/', methods=['POST'])
+@app.route('/songs/<int:eid>/edit')
+def edit_song(eid):
+    song = song_db.get(eid=eid)
+    return render_template('song.html', song=song)
+
+
+@app.route('/songs/<int:eid>/add_lyrics_section', methods=['POST'])
 def add_lyrics_section(eid):
     """
     Adds a lyrics section to the song.
     """
-    update_song_info(eid)
+    update_song_info(request=request, eid=eid)
     song = song_db.get(eid=eid)
     return render_template('song.html', song=song, add_section=True)
 
@@ -97,29 +124,40 @@ def remove_lyrics_section(eid, section_id):
     """
     Removes a lyric section from the song.
     """
-    print(eid, section_id)
-    update_song_info(eid, exclude_id=section_id)
+    update_song_info(request=request, eid=eid, exclude_id=section_id)
     song = song_db.get(eid=eid)
     return render_template('song.html', song=song, add_section=False)
 
 
-@app.route('/songs/<int:eid>/remove/', methods=['POST'])
-def delete_song(eid):
-    song_db.remove(eids=[eid])
+@app.route('/songs/clean', methods=['POST'])
+@app.route('/songs/clean')
+def clean_songs_database():
+    """
+    Cleans every entry in the songs database to match the Songs datamodel.
+    """
+    all_songs = song_db.all()
+    for s in all_songs:
+        for k, v in s.items():
+            if k == 'id':
+                pass
+            elif k not in song_datamodel:
+                song_db.update(delete(k), eids=[s.eid])
     return redirect('/songs')
 
 
-@app.route('/songs/<int:eid>/view/')
-@app.route('/songs/<int:eid>/')
-def view_song(eid):
-    song = song_db.get(eid=eid)
-    return render_template('song.html', song=song, add_section=False)
+@app.route('/coworkers', methods=['POST'])
+def view_coworkers():
+    return render_template('coworkers.html')
 
 
-@app.route('/songs/<int:eid>/edit/')
-def edit_song(eid):
-    song = song_db.get(eid=eid)
-    return render_template('song.html', song=song)
+@app.route('/coworkers/<int:id>/view', methods=['POST'])
+def view_coworker(id):
+    pass
+
+
+@app.route('/coworkers/<int:id>/edit', methods=['POST'])
+def edit_coworker(id):
+    pass
 
 
 if __name__ == '__main__':
--- datamodels.py
+++ datamodels.py
@@ -4,16 +4,30 @@ Data models used in the project.
 
 
 class Song(object):
-    def __init__(self, name, source, lyrics):
+    def __init__(self, name='name', copyright='organization',
+                 composer='composer', lyrics=None, ccli=None):
         self.name = name
-        self.source = source
-        assert isinstance(lyrics, Lyrics)
+        self.copyright = copyright
+        self.composer = composer
+        self.ccli = ccli
         self.lyrics = lyrics
+        self._add_lyrics(lyrics)
+
+    def _add_lyrics(self, lyrics):
+        if lyrics:
+            assert isinstance(lyrics, Lyrics)
+            self.lyrics = lyrics
+
+    def to_dict(self):
+        return self.__dict__
 
 
 class Lyrics(object):
     def __init__(self):
         self.sections = dict()
 
-    def add_section(self, section_name, lyrics):
-        self.sections[section_name] = lyrics
+    def add_section(self, section, lyrics):
+        self.sections[section] = lyrics
+
+    def to_dict(self):
+        return self.sections
