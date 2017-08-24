e152987ee0f064214815859e58bb73e79466a33b
Eric Ma <ericmajinglong@gmail.com>
2017-08-24 13:59:21 -0400

    Minor reduction in app's __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,4 @@
-import os.path as osp
-
 from flask import Flask, render_template
-
-from tinydb import TinyDB
-
-from .utils import makedir
 
 from .views import coworkers, programs, songs
 
@@ -12,16 +6,6 @@ app = Flask(__name__)
 app.register_blueprint(songs.mod)
 app.register_blueprint(coworkers.mod)
 app.register_blueprint(programs.mod)
-
-datafolder = 'data/'
-
-app.config['UPLOAD_FOLDER'] = osp.join(datafolder,
-                                       'files/')
-makedir(app.config['UPLOAD_FOLDER'])
-dbfolder = osp.join(datafolder, 'database')
-makedir(dbfolder)
-
-song_db = TinyDB(osp.join(dbfolder, 'song.db'))
 
 
 @app.route('/')
