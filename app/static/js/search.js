26626e4cee4e340fb4f1a00dae986781af8b8624
Eric Ma <ericmajinglong@gmail.com>
2017-12-02 14:37:58 -0500

    Expanded number of fields to search

--- app/static/js/search.js
+++ app/static/js/search.js
@@ -6,14 +6,13 @@ function search() {
 
   // Get active members' table rows.
   table = document.getElementById("table");
-  console.log(table)
   tr = table.getElementsByTagName("tbody")[0].children;
 
   // Loop through all table rows, and hide those who don't match the search query
   function find(tr) {
       for (i = 0; i < tr.length; i++) {
         tr[i].style.display = "none"
-        for (j = 0; j < 3; j++) {
+        for (j = 0; j < 5; j++) {
           td = tr[i].getElementsByTagName("td")[j];
           if (td) {
             if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
