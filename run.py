f6c4c6165c1170bd47c77999d6c56490ec0c0b80
Eric Ma <ericmajinglong@gmail.com>
2018-12-29 16:19:08 +0800

    removed unnecessary lines

--- run.py
+++ run.py
@@ -1,7 +1,4 @@
 from app import app
 
 if __name__ == "__main__":
-    app.secret_key = "super secret key"
-    app.config["SESSION_TYPE"] = "filesystem"
-
     app.run(debug=True, port=8080, host="0.0.0.0")
