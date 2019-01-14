04f1a6d22c84b9b51b084201adc296d92a5545c8
Eric Ma <ericmajinglong@gmail.com>
2019-01-13 21:47:45 -0500

    moved unnecessary imports out

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,23 @@
+import os
+from pathlib import Path
+
+import boto3
+import psycopg2
 from flask import Flask, render_template
+from hanziconv import HanziConv
+from tinydb import TinyDB
 
 from .views import songs
+from .config import DB_URL
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
 app.register_blueprint(songs.mod)
 # Register other blueprints later.
 
+app.config["SQLALCHEMY_DATABASE_URI"] = DB_URL
+db = SQLAlchemy(app)
 
 @app.route("/")
 def home():
